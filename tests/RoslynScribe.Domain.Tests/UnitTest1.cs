e61d58b5f124d008658d593b318a225c257be888
QuantumCores <kanonear@gmail.com>
2025-01-20 19:27:41 +0100

    QC: add unit test project

---
+++ tests/RoslynScribe.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RoslynScribe.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
